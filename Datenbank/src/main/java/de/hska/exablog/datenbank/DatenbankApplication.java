0e46c6daddfded8991795b60f85cbca395eabf03
dojo1017 <dojo1017@hs-karlsruhe.de>
2016-12-03 18:28:31 +0100

    * gradle build-vorgang optimiert / vereinfacht * Pakete erstellt

--- Datenbank/src/main/java/de/hska/exablog/datenbank/DatenbankApplication.java
+++ Datenbank/src/main/java/de/hska/exablog/datenbank/DatenbankApplication.java
@@ -1,13 +1,13 @@
-package de.hska.exablog;
+package de.hska.exablog.datenbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SpringWebApplication {
+public class DatenbankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebApplication.class, args);
+		SpringApplication.run(DatenbankApplication.class, args);
 	}
 }
