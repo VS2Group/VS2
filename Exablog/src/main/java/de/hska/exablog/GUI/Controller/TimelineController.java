a33f43efc3f41c959ef6c52fe90b73529d7f7339
dojo1017 <dojo1017@hs-karlsruhe.de>
2016-12-07 16:40:11 +0100

    Fixed #22

--- Exablog/src/main/java/de/hska/exablog/GUI/Controller/TimelineController.java
+++ Exablog/src/main/java/de/hska/exablog/GUI/Controller/TimelineController.java
@@ -1,10 +1,12 @@
 package de.hska.exablog.GUI.Controller;
 
 import de.hska.exablog.GUI.Controller.Data.PostData;
+import de.hska.exablog.Logik.Exception.UserDoesNotExistException;
 import de.hska.exablog.Logik.Model.Entity.Session;
 import de.hska.exablog.Logik.Model.Entity.User;
 import de.hska.exablog.Logik.Model.Service.SessionService;
 import de.hska.exablog.Logik.Model.Service.TimelineService;
+import de.hska.exablog.Logik.Model.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +27,8 @@ public class TimelineController {
 	private SessionService sessionService;
 	@Autowired
 	private TimelineService timelineService;
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getTimeline(HttpSession session, Model model) {
@@ -33,6 +37,7 @@ public class TimelineController {
 			return "redirect:/login";
 		}
 
+		model.addAttribute("timelinetype", "global");
 		model.addAttribute("postData", new PostData());
 		model.addAttribute("user", user);
 		model.addAttribute("timeline", timelineService.getGlobalTimeline());
@@ -42,14 +47,24 @@ public class TimelineController {
 
 	@RequestMapping(value = "/{username}", method = RequestMethod.GET)
 	public String getTimeline(@PathVariable("username") String username, HttpSession session, Model model) {
-		User user = sessionService.validateSession(session.getId());
-		if (user == null) {    // User ist eingeloggt
+		User thisUser = sessionService.validateSession(session.getId());
+		if (thisUser == null) {    // User ist eingeloggt
 			return "redirect:/login";
 		}
 
+		User timelineUser = null;
+		try {
+			timelineUser = userService.getUserByName(username);
+		} catch (UserDoesNotExistException e) {
+			return "redirect:/timeline";
+		}
+
+
+		model.addAttribute("timelinetype", "private");
 		model.addAttribute("postData", new PostData());
-		model.addAttribute("user", user);
-		model.addAttribute("timeline", timelineService.getPersonalTimeline(user));
+		model.addAttribute("user", thisUser);
+		model.addAttribute("timeline", timelineService.getPersonalTimeline(timelineUser));
+		model.addAttribute("timelineUser", timelineUser);
 
 		return "timeline";
 	}
